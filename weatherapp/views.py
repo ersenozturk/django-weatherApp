23ddd2523e66db30b6b40397a1e8b8f2123dab50
ersen <70ozturkersen@gmail.com>
2022-06-10 15:22:38 +0300

    revised

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -9,14 +9,15 @@ from weatherapp.models import City
 
 def base(request):
     API_key = config('API_KEY')
-    u_city = request.GET.get('name')  #cityName model den geldi
+    u_city = request.GET.get('name')  # cityName model den geldi
     # print(u_city)
     if u_city:
         url = f'https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_key}&units=metric&'
         response = requests.get(url)
         if response.ok:  # 400 ün altında true olarak döner
             cityInfo = response.json()  # python dict e döndürmek için
-            r_city = cityInfo['name']   # cityInfo içindeki  name api denn geldi 
+            # cityInfo içindeki  name api denn geldi
+            r_city = cityInfo['name']
             if City.objects.filter(name=r_city):
                 messages.warning(request, 'city already existts!')
             else:
@@ -33,33 +34,36 @@ def base(request):
     # pprint(cityInfo['weather'][0]['description'])
     # pprint(cityInfo['weather'][0]['icon'])
 
-
-
     #! db den çekip frontend e gönderme kısmı aşağısı
     all_city_data = []
-    cities = City.objects.all() # db deki tüm şehirleri çek
+    cities = City.objects.all()  # db deki tüm şehirleri çek
 
     for each_city in cities:
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={each_city}&appid={API_key}&units=metric&' # db deki bütün cityleri döngüye aldık
+        # db deki bütün cityleri döngüye aldık
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={each_city}&appid={API_key}&units=metric&'
         response = requests.get(url)
         cityInfo = response.json()
         data = {
             # 'city' : cityInfo['name'],
-            'city' : each_city,
-            'temp' : cityInfo['main']['temp'],
-            'desc' : cityInfo['weather'][0]['description'],
-            'icon' : cityInfo['weather'][0]['icon']
+            # 'city': each_city,
+            'temp': cityInfo['main']['temp'],
+            'desc': cityInfo['weather'][0]['description'],
+            'icon': cityInfo['weather'][0]['icon'],
+
+            "city": cityInfo['name'],
+            "id": each_city.id,
         }
         all_city_data.append(data)
         # pprint(all_city_data)
-    
-    context = {
-        'all_city_data':all_city_data,
-    }
-    return render(request, 'weatherapp/base.html',context)
 
-def delete_city(request,id):
-    city = get_object_or_404(City,id=id)
+    context = {
+        'all_city_data': all_city_data,
+    }
+    return render(request, 'weatherapp/base.html', context)
+
+
+def delete_city(request, id):
+    city = get_object_or_404(City, id=id)
     city.delete()
     messages.success(request, 'city deleted')
     return redirect('base')
